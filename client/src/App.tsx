437cdac0ba3a47a5af99b99d682846c63681590a
SamahaMunir <119500740+SamahaMunir@users.noreply.github.com>
2026-02-12 19:00:51 +0500

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/projects" component={Projects} />
+          <Route path="/experience" component={Experience} />
           <Route path="/contact" component={Contact} />
         </Switch>
       </main>
